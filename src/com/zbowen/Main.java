bf15df1d90a59166e4735376840e392a899040e8
zbw <1627974279@qq.com>
2020-09-11 18:53:40 +0800

    自定义实现ArrayList

---
+++ src/com/zbowen/Main.java
@@ -0,0 +1,22 @@
+package com.zbowen;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArrayList<Person> list = new ArrayList<>();
+		Person person = new Person(15, "xiaohong");
+		list.add(person);
+		list.add(new Person(12, "jack"));
+		list.add(new Person(18, "Tom"));
+		list.add(new Person(16, "lucy"));
+		list.set(2, new Person(20, "小米"));
+		list.add(3, new Person(24, "华为"));
+		System.out.println(list.remove(0));
+		System.out.println(list.contains(person));
+		System.out.println(list);
+		list.clear();
+		System.gc();
+		
+	}
+
+}
